da158cb10f762619f061545be2441e21f92bcb7e
toiosoiovoio <rpdelgado@gmail.com>
2023-11-17 22:12:28 +0000

    Update now.py

--- now.py
+++ now.py
@@ -16,7 +16,7 @@ def now(update: Update, context: CallbackContext) -> None:
 
 def main() -> None:
     # Set up the Telegram Bot with your token
-    updater = Updater("6321549495:AAE-shnQcCl5_MwBqfj9_2LvQIu3z_kbqaM")
+    updater = Updater("")
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
